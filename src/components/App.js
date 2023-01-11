9bc51c6deb63e86560e602d435c6f4f0d3ebe4a4
Daniel da Silva <danielfdsilva@gmail.com>
2023-01-11 12:00:43 +0000

    Restructure for v2

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const allRuns = [
+  {id: 'morningRun', label: ''},
+  {id: 'eveRun', label: ''},
+  {id: 'sundayRun', label: ''},
+];
+
+export default function App() {
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  );
+}
