601739777491af5f8f60eb498101fe1152829977
Daniel da Silva <danielfdsilva@gmail.com>
2023-01-18 11:31:22 +0000

    Move runs to separate file

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,5 @@
 import React from "react";
-
-const allRuns = [
-  { id: "forestRun", label: "Forest Run", date: "2023-1-10" },
-  { id: "lunchRun", label: "Lunch Run", date: "2022-11-26" },
-  { id: "marathon", label: "Marathon", date: "2021-11-7" },
-];
+import { allRuns } from "../data";
 
 export default function App() {
   return (
---
+++ src/data/index.js
@@ -0,0 +1,5 @@
+export const allRuns = [
+  { id: "forestRun", label: "Forest Run", date: "2023-1-10" },
+  { id: "lunchRun", label: "Lunch Run", date: "2022-11-26" },
+  { id: "marathon", label: "Marathon", date: "2021-11-7" },
+];
