9544c89803785eb23fb9dafd74c708dc9750d482
Will Rynearson <will@developmentseed.org>
2023-02-02 10:02:36 +0100

    got the sorting to work, but it's mutable

--- src/components/App.js
+++ src/components/App.js
@@ -11,9 +11,14 @@ export default function App() {
   });
 
   // trying to copy allRuns without mutating it
-  const runsSorted = runs.sort(function (a, b) {
-    return b.date - a.date;
-  });
+  let runsSorted = runs.sort((a, b) =>
+    a.date < b.date ? -1 : a.date > b.date ? -1 : 0
+  );
+
+  // this works to change the order by date, but it's mutable...
+  let runsSorted2 = allRuns.sort((a, b) =>
+    a.date < b.date ? 1 : a.date > b.date ? -1 : 0
+  );
 
   // testing if my sort function logic works
   let testing = [1, 4, -5, 6, 89].sort(function (a, b) {
@@ -27,12 +32,14 @@ export default function App() {
   // seeing what's passed to the browser
   console.log("allRuns: ", allRuns);
   console.log("runsSorted: ", runsSorted);
+  console.log("runsSorted2: ", runsSorted2);
 
   return (
     <div>
       <ul>{runs}</ul>
       <ol>{runsSorted}</ol>
       <ul>{testing}</ul>
+      {runsSorted}
     </div>
   );
 }
