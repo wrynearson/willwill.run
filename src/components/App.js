44bf90abfa11a17acf83bef4cacb12601abc5027
Daniel da Silva <danielfdsilva@gmail.com>
2023-02-02 11:21:34 +0000

    Extract runs sort function

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { allRuns } from "../data";
 
+function sortRunsByDate(list, order) {
+  // list [run, run, run] -- run: { id, label, date }
+  const listSorted = [...list].sort((a, b) =>
+    a.date > b.date ? -1 : a.date < b.date ? 1 : 0
+  );
+  return listSorted;
+}
+
 export default function App() {
   const runs = allRuns.map(function (run, index) {
     return (
@@ -11,34 +19,25 @@ export default function App() {
   });
 
   // trying to copy allRuns without mutating it
-  let runsSorted = [...runs].sort((a, b) =>
-    a.date < b.date ? 1 : a.date > b.date ? -1 : 0
-  );
-
-  // this works to change the order by date, but it's mutable...
-  let runsSorted2 = allRuns.sort((a, b) =>
-    a.date < b.date ? 1 : a.date > b.date ? -1 : 0
-  );
-
-  // testing if my sort function logic works
-  let testing = [1, 4, -5, 6, 89].sort(function (a, b) {
-    return a - b;
-  });
-
-  // testing if I can iterate over items in testing and allRuns
-  testing.forEach((test) => console.log(test));
-  allRuns.forEach((id) => console.log(id.date));
+  const runsSorted = sortRunsByDate(allRuns);
 
   // seeing what's passed to the browser
   console.log("allRuns: ", allRuns);
   console.log("runs: ", runs);
   console.log("runsSorted: ", runsSorted);
-  // console.log("runsSorted2: ", runsSorted2);
 
   return (
     <div>
       <ul>{runs}</ul>
-      <ol>{runsSorted}</ol>
+      <ol>
+        {runsSorted.map(function (run, index) {
+          return (
+            <li key={run.id}>
+              {run.label}, {run.date}
+            </li>
+          );
+        })}
+      </ol>
     </div>
   );
 }
