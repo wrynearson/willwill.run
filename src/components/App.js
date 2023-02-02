11670e515f4bd96807f5da0748a39ffa3471e5c5
Will Rynearson <will@developmentseed.org>
2023-02-02 09:29:52 +0100

    sorting and testing

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,36 @@ import { allRuns } from "../data";
 
 export default function App() {
   const runs = allRuns.map(function (run, index) {
-    return <li key={run.id}>{run.label}</li>;
+    return (
+      <li key={run.id}>
+        {run.label}, {run.date}
+      </li>
+    );
   });
+
+  // trying to copy allRuns without mutating it
+  const runsSorted = runs.sort(function (a, b) {
+    return b.date - a.date;
+  });
+
+  // testing if my sort function logic works
+  let testing = [1, 4, -5, 6, 89].sort(function (a, b) {
+    return a - b;
+  });
+
+  // testing if I can iterate over items in testing and allRuns
+  testing.forEach((test) => console.log(test));
+  allRuns.forEach((id) => console.log(id.date));
+
+  // seeing what's passed to the browser
+  console.log("allRuns: ", allRuns);
+  console.log("runsSorted: ", runsSorted);
 
   return (
     <div>
       <ul>{runs}</ul>
+      <ol>{runsSorted}</ol>
+      <ul>{testing}</ul>
     </div>
   );
 }
--- src/data/index.js
+++ src/data/index.js
@@ -1,5 +1,5 @@
 export const allRuns = [
+  { id: "marathon", label: "Marathon", date: "2021-11-7" },
   { id: "forestRun", label: "Forest Run", date: "2023-1-10" },
   { id: "lunchRun", label: "Lunch Run", date: "2022-11-26" },
-  { id: "marathon", label: "Marathon", date: "2021-11-7" },
 ];
