883022ae3f715f6975fa3aef49c18efaf30faed8
Daniel da Silva <danielfdsilva@gmail.com>
2023-02-08 11:34:04 +0000

    Implement list ordering

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { allRuns } from "../data";
 
-function sortRunsByDate(list, order = -1) {
+function sortRunsByDate(list, order = "asc") {
+  const order2 = order === "asc" ? 1 : order === "desc" ? -1 : 0;
   // list [run, run, run] -- run: { id, label, date }
   const listSorted = [...list].sort((a, b) =>
-    a.date > b.date ? -1 * order : a.date < b.date ? 1 * order : 0
+    a.date > b.date ? -1 * order2 : a.date < b.date ? 1 * order2 : 0
   );
   return listSorted;
 }
@@ -20,6 +21,8 @@ export default function App() {
 
   // trying to copy allRuns without mutating it
   const runsSorted = sortRunsByDate(allRuns);
+  const runsDesc = sortRunsByDate(allRuns, "desc");
+  const runsUnsorted = sortRunsByDate(allRuns, "Daniel");
 
   // seeing what's passed to the browser
   console.log("allRuns: ", allRuns);
@@ -31,6 +34,24 @@ export default function App() {
       <ul>{runs}</ul>
       <ol>
         {runsSorted.map(function (run, index) {
+          return (
+            <li key={run.id}>
+              {run.label}, {run.date}
+            </li>
+          );
+        })}
+      </ol>
+      <ol>
+        {runsDesc.map(function (run, index) {
+          return (
+            <li key={run.id}>
+              {run.label}, {run.date}
+            </li>
+          );
+        })}
+      </ol>
+      <ol>
+        {runsUnsorted.map(function (run, index) {
           return (
             <li key={run.id}>
               {run.label}, {run.date}
