451e0f7a86c69258e7038c42c2583dd4a9a07f06
Will Rynearson <will@developmentseed.org>
2023-08-23 11:01:31 +0200

    cleaning

--- src/components/App.js
+++ src/components/App.js
@@ -189,16 +189,13 @@ export default function App() {
                 url: "mapbox://mapbox.mapbox-terrain-dem-v1",
                 maxzoom: 14,
               });
-              // add the DEM source as a terrain layer with exaggerated height
               map.target.setTerrain({
                 source: "mapbox-dem",
                 exaggeration: 1,
               });
             }}
             onMove={(evt) => setViewport(evt.viewport)}
-            // style={{ width: 600, height: 400 }}
             mapStyle="mapbox://styles/mapbox/outdoors-v11"
-            setTerrain={{ source: "mapbox-dem", exaggeration: 1.5 }}
           >
             {fetchedRun !== undefined ? (
               <Source id="my-data" type="geojson" data={fetchedRun.feature}>
