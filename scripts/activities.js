00acd9cda1f1e7a59b0f2c7528c40db91f039111
Will Rynearson <will@developmentseed.org>
2023-12-20 12:27:19 +0100

    fetch all activities, details for one activity

--- scripts/activities.js
+++ scripts/activities.js
@@ -4,12 +4,12 @@ const fs = require("fs");
 
 const credentials = require(`${__dirname}/../credentials.json`);
 
-strava.config({
-  access_token: credentials.access_token,
-  client_id: credentials.client_id,
-  client_secret: credentials.client_secret,
-  redirect_uri: "https://www.strava.com/oauth/authorize",
-});
+// strava.config({
+//   access_token: credentials.access_token,
+//   client_id: credentials.client_id,
+//   client_secret: credentials.client_secret,
+//   redirect_uri: "https://www.strava.com/oauth/authorize",
+// });
 
 async function getActivities() {
   try {
@@ -35,7 +35,30 @@ async function getActivities() {
       access_token: credentials.access_token,
       id: credentials.athlete.id,
     });
-    console.log(activities);
+    //console.log(activities);
+
+    fs.writeFileSync(
+      `${__dirname}/../data-prep/activities.json`,
+      JSON.stringify(activities)
+    );
+
+    const activity = await strava.streams.activity({
+      access_token: credentials.access_token,
+      id: 10394935893,
+      types: [
+        "distance",
+        "altitude",
+        "latlng",
+        "time",
+        "heartrate",
+        "velocity_smooth",
+        "watts",
+        "temp",
+        "cadence",
+      ],
+    });
+
+    console.log(activity);
   } catch (error) {
     console.log(error);
   }
--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,5 @@
+// http://www.strava.com/oauth/authorize?client_id=[CLIENT_ID]&response_type=code&redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=read_all,activity:read_all
+
 const fs = require("fs");
 
 async function getCredentials() {
