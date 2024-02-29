fd9af0211814b16a685941eb72a6d9425b17da32
Will Rynearson <will@developmentseed.org>
2024-02-29 12:53:52 -0800

    update

--- scripts/transform.js
+++ scripts/transform.js
@@ -3,17 +3,18 @@ function transformActivity(activityMeta, activityDetails) {
     id: activityMeta.id,
     name: activityMeta.name,
     // run could be a street run or trail run. Could be useful to group all runs (street and trail together)
-    activity_theme: activityMeta.type,
+    activity_type: activityMeta.type,
     // trail run is specified
-    activity_type: activityMeta.sport_type,
+    activity_sport_type: activityMeta.sport_type,
     start_time: activityMeta.start_date_local,
     timezone: activityMeta.timezone,
     utc_offset: activityMeta.utc_offset,
     country: activityMeta.location_country,
     // "only me" = private, "followers_only" means semi-private, "everyone" = public
     visibility: activityMeta.visibility,
-
     distance: activityMeta.distance,
+    moving_time: activityMeta.moving_time,
+    elapsed_time: activityMeta.elapsed_time,
 
     polyline: activityMeta.map.summary_polyline,
 
