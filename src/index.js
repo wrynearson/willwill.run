61114edb1be56fc88d89679b0c5b3f997731ce99
Daniel da Silva <daniel@developmentseed.org>
2024-03-27 12:57:35 +0000

    Add router basepath

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<App />} />
         {/* * should be the last path in the list */}
