55bd448f9619942464aae122ebb4eb367973fdec
Daniel da Silva <danielfdsilva@gmail.com>
2024-07-11 09:47:15 +0100

    Fix page path

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from "./components/App";
 import "./index.css";
-import About from "./pages/About";
-import NotFound from "./pages/NotFound";
+import About from "./pages/About/About";
+import NotFound from "./pages/NotFound/NotFound";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
